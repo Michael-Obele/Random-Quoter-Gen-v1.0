435390158d1acc73edd4433a5b6b142aa8b4861f
Michael Obubelebra Amachree <70345027+Michael-Obele@users.noreply.github.com>
2022-04-27 23:49:47 +0100

    Changes

--- index.js
+++ index.js
@@ -455,6 +455,18 @@ $(document).ready(function () {
 });
 // End of Redux Function
 
+$(document).ready(function () {
+  async;
+  src = 'https://www.googletagmanager.com/gtag/js?id=G-MJKVGLHWNY';
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-MJKVGLHWNY');
+});
 //
 //
 //
